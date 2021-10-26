2d7fb59f62b046d8943f3e6ff37d99fbf81e2355
Alex Bethel <abethel@cs.nmt.edu>
2021-10-26 12:03:20 -0600

    Slight cleanups

--- ros/robot_kbd/src/controller.cpp
+++ ros/robot_kbd/src/controller.cpp
@@ -64,53 +64,54 @@ void KeyboardReader::readOne(char *c) {
     throw std::runtime_error("read failed");
   }
 #else
-  for (;;) {
+  while (1) {
     HANDLE handle = GetStdHandle(STD_INPUT_HANDLE);
     INPUT_RECORD buffer;
     DWORD events;
     PeekConsoleInput(handle, &buffer, 1, &events);
     if (events > 0) {
       ReadConsoleInput(handle, &buffer, 1, &events);
-      if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_LEFT) {
+      switch (buffer.Event.KeyEvent.wVirtualKeyCode) {
+      case VK_LEFT:
         *c = KEYCODE_LEFT;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_UP) {
+      case VK_UP:
         *c = KEYCODE_UP;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_RIGHT) {
+      case VK_RIGHT:
         *c = KEYCODE_RIGHT;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == VK_DOWN) {
+      case VK_DOWN:
         *c = KEYCODE_DOWN;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x42) {
+      case 0x42:
         *c = KEYCODE_B;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x43) {
+      case 0x43:
         *c = KEYCODE_C;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x44) {
+      case 0x44:
         *c = KEYCODE_D;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x45) {
+      case 0x45:
         *c = KEYCODE_E;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x46) {
+      case 0x46:
         *c = KEYCODE_F;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x47) {
+      case 0x47:
         *c = KEYCODE_G;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x51) {
+      case 0x51:
         *c = KEYCODE_Q;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x52) {
+      case 0x52:
         *c = KEYCODE_R;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x54) {
+      case 0x54:
         *c = KEYCODE_T;
         return;
-      } else if (buffer.Event.KeyEvent.wVirtualKeyCode == 0x56) {
+      case 0x56:
         *c = KEYCODE_V;
         return;
       }
