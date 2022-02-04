3ff87d7fff225cf457b685693b1583ff105bff66
shastro <guitarshughes@hotmail.com>
2022-02-04 13:55:36 -0700

    Add webcam support for arucoTest

--- user/shughes/arucoTest/aruco_simple.cpp
+++ user/shughes/arucoTest/aruco_simple.cpp
@@ -5,20 +5,46 @@
 using namespace std;
 int main(int argc,char **argv)
 {
-   cv::Mat im=cv::imread(argv[1]);
+   cv::Mat im;
 
-   // aruco::CameraParameters camera;
-   // camera.readFromXMLFile(argv[2]);
-   // aruco::MarkerDetector Detector;
-   // Detector.setDictionary("ARUCO_MIP_36h12");
-   // auto markers=Detector.detect(im,camera,0.05);//0.05 is the marker size
-   // for(auto m:markers){
-   //     aruco::CvDrawingUtils::draw3dAxis(im,m,camera);
-   //     cout<<m.Rvec<<" "<<m.Tvec<<endl;
-   // }
-   cv::imshow("image",im);
-   //cout << "bruh\n"; 
-   //cv::waitKey(0);
+
+    cv::VideoCapture cap;
+
+
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+
+
+    // open selected camera using selected API
+    cap.open(deviceID, apiID);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+
+
+   aruco::CameraParameters camera;
+   camera.readFromXMLFile(argv[1]);
+
+
+
+   aruco::MarkerDetector Detector;
+   Detector.setDictionary("ARUCO_MIP_36h12");
+
+   for (;;) {
+      cap.read(im);
+      auto markers=Detector.detect(im,camera,0.07);//0.05 is the marker size
+
+      for(auto m:markers){
+          aruco::CvDrawingUtils::draw3dAxis(im,m,camera);
+          cout<<m.Rvec<<" "<<m.Tvec<<endl;
+      }
+
+      cv::imshow("image",im);
+
+      // cv::waitKey(0);
+   }
 }
 
 
