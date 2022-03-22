9e83c314e5e74703ce493794a12aa2cbccc2fb8d
Alex Bethel <abethel@cs.nmt.edu>
2022-03-21 21:32:10 -0600

    Set up `sim_basic.sh` with the laser
    Mapping seems to work again :)

--- scripts/sim_basic.sh
+++ scripts/sim_basic.sh
@@ -9,15 +9,20 @@ set -e
 
 cd $(dirname "$0")
 
+# wait_for_ros : Waits for ROS to launch.
+wait_for_ros () {
+    while ! rosservice list > /dev/null 2>&1; do
+        echo 'waiting for ROS'
+        sleep 1
+    done
+}
+
 # wait_and_call <ros_service> <params ...> : Waits for the ROS service
 # to exist, then calls it, avoiding startup race conditions.
 wait_and_call () {
     # ROS has to be initialized; wait for `rosservice` to successfully
     # connect.
-    while ! rosservice list > /dev/null; do
-        echo 'waiting for ROS'
-        sleep 1
-    done
+    wait_for_ros
 
     # The service has to exist.
     while ! rosservice list | grep -q "^$1\$"; do
@@ -29,13 +34,17 @@ wait_and_call () {
     rosservice call "$@"
 }
 
+# Start up ROS.
 . ../ros/catkin_ws/devel/setup.sh
 roslaunch david_conf temp_sim.launch robot_path:=david_sim &
-webots ../webots/worlds/lunabotics_ai.wbt &
+
+# Connect the Webots world.
+wait_for_ros
+webots ../webots/worlds/lunabotics_TEST_FLAT.wbt &
 
 # Enable the lidar.
-wait_and_call /david_sim/Hokuyo_URG_04LX/enable 64
-wait_and_call /david_sim/Hokuyo_URG_04LX/enable_point_cloud 64
+wait_and_call /david_sim/laser/enable 64
+wait_and_call /david_sim/laser/enable_point_cloud 64
 
 # Start up RVIZ; use `true` because we still need to clean up, even if
 # it fails.
