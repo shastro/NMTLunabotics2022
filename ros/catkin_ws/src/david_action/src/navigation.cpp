cd27cba0d27a5c775b4751eb88065a767e2ef68e
Alex Bethel <abethel@cs.nmt.edu>
2022-05-11 13:58:34 -0600

    More navigation server skeleton code

--- ros/catkin_ws/src/david_action/src/navigation.cpp
+++ ros/catkin_ws/src/david_action/src/navigation.cpp
@@ -1,8 +1,34 @@
+#include <functional>
 #include <iostream>
+
+#include <actionlib/server/simple_action_server.h>
+#include <david_action/NavigationAction.h>
+#include <ros/node_handle.h>
 #include <ros/ros.h>
 
 using namespace std;
 
+class NavigationAction {
+  ros::NodeHandle _nh;
+
+  actionlib::SimpleActionServer<david_action::NavigationAction> _as;
+  string _action_name;
+
+  void executeCB(const david_action::NavigationGoalConstPtr &goal) {
+    const david_action::NavigationGoal &pgoal = *goal;
+  }
+
+public:
+  NavigationAction(string name)
+      : _as(_nh, name, bind(&NavigationAction::executeCB, this, _1), false),
+        _action_name(name) {
+    _as.start();
+  }
+};
+
 int main(int argc, char **argv) {
-  cout << "Hello World!" << endl;
+  ros::init(argc, argv, "navigation");
+
+  NavigationAction nav("navigation");
+  ros::spin();
 }
