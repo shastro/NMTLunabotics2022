30b8a0599a89dd40f0afc66af5cc86c81f31c5fc
shastro <guitarshughes@hotmail.com>
2022-05-11 21:08:35 -0600

    Add test workspace

--- user/shughes/test_workspace/build/CTestTestfile.cmake
+++ user/shughes/test_workspace/build/CTestTestfile.cmake
@@ -5,7 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("teb_local_planner_tutorials")
 subdirs("turtlebot3_simulations/turtlebot3_simulations")
-subdirs("action_tutorials")
 subdirs("turtlebot3_simulations/turtlebot3_fake")
 subdirs("turtlebot3_simulations/turtlebot3_gazebo")
