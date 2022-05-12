f1a2cfe8eaa508e3e8309ea5c8e427be0d8c2489
Alex Bethel <abethel@cs.nmt.edu>
2022-05-11 20:28:44 -0600

    Teknic ROS integration

--- ros/catkin_ws/src/base_controller/src/teknic.cpp
+++ ros/catkin_ws/src/base_controller/src/teknic.cpp
@@ -13,101 +13,37 @@
 
 #include "main.hpp"
 #include "teknic.hpp"
+#include "teknic/motor_utils.hpp"
 
 using namespace sFnd;
 using namespace std;
 
-#define TODO abort()
-
 // Construct a motor controller at the given path, and initialize it
 // to zero velocity.
-TeknicMotor::TeknicMotor() { TODO; }
+TeknicMotor::TeknicMotor(SimpleNode &node) : _node(node) {}
 
-void TeknicMotor::setVelocity(double vel) { TODO; }
+void TeknicMotor::setVelocity(double vel) { _node.setVel(vel); }
 
-void TeknicMotor::setPosition(double pos) { TODO; }
+void TeknicMotor::setPosition(double pos) { _node.setPos(pos); }
 
 // Initializes the navigation motors for the robot.
 vector<NavMotor> init_motors(string path) {
+  vector<SimplePort> ports = SimplePort::getPorts();
+
+  // This leaks `ports[0]` on purpose, because the `SimplePort` has to
+  // be alive for the `SimpleNode`s to function. Ideally we would
+  // change the return type here to be some type that keeps the
+  // `SimplePort` alive, but I don't have time.
+  SimplePort *port = new SimplePort(move(ports[0]));
+
+  vector<SimpleNode> &nodes = port->getNodes();
   vector<NavMotor> motors;
 
-  SysManager *mgr = SysManager::Instance();
-  try {
-    size_t port_count = setup_ports(mgr);
-
-    for (size_t i = 0; i < port_count; i++) {
-      IPort &port = mgr->Ports(i);
-      cout << "Port " << port.netNumber() << ": state = " << port.openState()
-           << ", nodes = << " << port.NodeCount() << endl;
-      for (size_t node_num = 0; node_num < port.NodeCount(); node_num++) {
-        INode &node = port.Nodes(node_num);
-
-        // Ensure node is disabled before loading config file.
-        node.EnableReq(false);
-
-        mgr->Delay(200);
-
-        cout << "Node " << node_num << ": type = " << node.Info.NodeType()
-             << endl
-             << "UserID: " << node.Info.UserID.Value() << endl
-             << "FW version: " << node.Info.FirmwareVersion.Value() << endl
-             << "Serial: " << node.Info.SerialNumber.Value() << endl
-             << "Model: " << node.Info.Model.Value() << endl;
-
-        // Enable the node.
-        node.Status.AlertsClear();
-        node.Motion.NodeStopClear();
-        node.EnableReq(true);
-
-        cout << "Node " << node_num << " enabled." << endl;
-
-        double timeout = mgr->TimeStampMsec() + TIME_TILL_TIMEOUT;
-        while (!node.Motion.IsReady()) {
-          if (mgr->TimeStampMsec() > timeout) {
-            cerr << "Timed out waiting for node " << node_num << " to enable."
-                 << endl;
-            abort();
-          }
-
-          usleep(POLL_INTERVAL * 1000);
-        }
-
-        // Would home motors here; but according to documentation
-        // (IHoming description):
-        // > Homing is required to use soft limits and establish the
-        // > node's number space for absolute type moves.
-        // Given that we're not using soft limits or absolute type
-        // moves, we don't care about homing.
-        TODO;
-      }
-      TODO;
-    }
-    TODO;
-  } catch (mnErr &err) {
-    TODO;
-  }
+  // And this leaks TeknicMotor objects. Cry about it.
+  motors.push_back(
+      NavMotor(new TeknicMotor(nodes.at(MotorIdent::LocomotionL)), -1));
+  motors.push_back(
+      NavMotor(new TeknicMotor(nodes.at(MotorIdent::LocomotionR)), 1));
 
   return motors;
-}
-
-// Sets up the ports on the SysManager; returns the number of ports
-// initialized.
-static size_t setup_ports(SysManager *mgr) {
-  vector<string> com_hub_ports;
-  SysManager::FindComHubPorts(com_hub_ports);
-  cout << "Debug: found " << com_hub_ports.size() << " hubs" << endl;
-
-  if (com_hub_ports.size() == 0) {
-    cerr << "No SC hub port found." << endl;
-    abort();
-  }
-
-  int port_count = min(com_hub_ports.size(), NET_CONTROLLER_MAX);
-  for (int i = 0; i < port_count; i++) {
-    mgr->ComHubPort(i, com_hub_ports[i].c_str());
-  }
-
-  mgr->PortsOpen(port_count);
-
-  return port_count;
 }
