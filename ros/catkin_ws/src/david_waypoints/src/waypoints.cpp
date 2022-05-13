e1425b46a2cfe015d3abdd0f327c3f763ad279af
Alex Bethel <abethel@cs.nmt.edu>
2022-05-13 15:05:57 -0600

    Waypoints skeleton work

--- ros/catkin_ws/src/david_waypoints/src/waypoints.cpp
+++ ros/catkin_ws/src/david_waypoints/src/waypoints.cpp
@@ -1,4 +1,31 @@
+#include "ros/node_handle.h"
+#include "ros/publisher.h"
 #include <iostream>
 #include <ros/ros.h>
+#include <sensor_msgs/Image.h>
 
-int main(int argc, char **argv) {}
+using namespace std;
+
+class WaypointCalculator {
+public:
+  WaypointCalculator() {
+    _nh.subscribe("/camera_image", 1,
+                  &WaypointCalculator::camera_image_callback, this);
+  }
+
+  void camera_image_callback(sensor_msgs::ImageConstPtr &ros_image) {
+  }
+
+private:
+  ros::NodeHandle _nh;
+  // TODO: Waypoint position publishers.
+};
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "waypoints");
+
+  WaypointCalculator calc;
+  ros::spin();
+
+  ros::shutdown();
+}
