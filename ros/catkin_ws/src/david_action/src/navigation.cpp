29adb51c554dc9b609d4cbe14fe2fe7fff958cf9
Alex Bethel <abethel@cs.nmt.edu>
2022-05-13 00:16:48 -0600

    Navigation server implementation skeleton

--- ros/catkin_ws/src/david_action/src/navigation.cpp
+++ ros/catkin_ws/src/david_action/src/navigation.cpp
@@ -3,6 +3,7 @@
 
 #include <actionlib/server/simple_action_server.h>
 #include <david_action/NavigationAction.h>
+#include <move_base_msgs/MoveBaseGoal.h>
 #include <ros/node_handle.h>
 #include <ros/ros.h>
 
@@ -15,7 +16,42 @@ class NavigationAction {
   string _action_name;
 
   void executeCB(const david_action::NavigationGoalConstPtr &goal) {
-    const david_action::NavigationGoal &pgoal = *goal;
+    // Need to publish to the navigation goal system. Then spin
+    // reading from the robot base frame until it is within a
+    // tolerance of the navigation goal. While we spin, publish
+    // progress reports.
+
+    {
+      // Tell move_base to navigate to the goal.
+      ros::Publisher goal_pub =
+          _nh.advertise<geometry_msgs::Pose>("/move_base/goal", 1000);
+      geometry_msgs::Pose pos = goal->target;
+      goal_pub.publish(pos);
+    }
+
+    // Wait for move_base to finish.
+    ros::Rate r = 5;            // hz
+    while (true) {
+      if (_as.isPreemptRequested() || !ros::ok()) {
+        cout << _action_name << ": Preempted" << endl;
+
+        _as.setPreempted();
+        return;
+      }
+
+      // TODO: Publish feedback.
+
+      // TODO: detect conclusion.
+
+      r.sleep();
+    }
+
+    cout << _action_name << ": Succeeded" << endl;
+
+    // TODO: measure this error correctly.
+    david_action::NavigationResult result;
+    result.error = 0;
+    _as.setSucceeded(result);
   }
 
 public:
