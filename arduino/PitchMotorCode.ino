b09f84ce37a05e22c2bf3a22e6d420de909ea192
Joseph Coston <josephacoston@gmail.com>
2022-05-14 00:25:27 -0600

    Made digitalRead pins in ino code verbose constants.

--- arduino/PitchMotorCode.ino
+++ arduino/PitchMotorCode.ino
@@ -2,168 +2,197 @@
  *
  */
 
-//The following define the pins for the lab
-#define in1 9
-#define in2 10
-#define in3 11
-#define in4 12
-#define pitchL 20
-#define pitchR 21
-#define bPin 3
+// The following define the pins for the lab
+#define HBridge_in1 8
+#define HBridge_in2 9
+#define HBridge_in3 10
+#define HBridge_in4 11
+
+#define homePin     4
+#define extendPin   5
+#define retractPin  6
+#define halfExtPin  12
+
+#define pitchL  2
+#define pitchR  3
+#define bPin    A2
 
 int encoder = 0;
 int encoderL = 0;
 int encoderlast = 1;
-int state = 0; //Colapsed = 0, extend half = 1, extend full = 2
+int state = 0; // Colapsed = 0, extend half = 1, extend full = 2
 int eStop = LOW;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  pinMode(HBridge_in1, OUTPUT);
+  pinMode(HBridge_in2, OUTPUT);
+  pinMode(HBridge_in3, OUTPUT);
+  pinMode(HBridge_in4, OUTPUT);
   pinMode(bPin, INPUT_PULLUP);
-  pinMode(23, INPUT); //Home
-  pinMode(25, INPUT); //Extend
-  pinMode(27, INPUT); //Retract
-  pinMode(29, INPUT); //1/2 Extend
+  pinMode(4, INPUT);  // Home
+  pinMode(5, INPUT);  // Extend
+  pinMode(6, INPUT);  // Retract
+  pinMode(12, INPUT); // 1/2 Extend
   attachInterrupt(digitalPinToInterrupt(pitchL), c, RISING);
   attachInterrupt(digitalPinToInterrupt(pitchR), cL, RISING);
   attachInterrupt(digitalPinToInterrupt(bPin), button, HIGH);
-
-  //Inturrupt for encoder A used to detect changes in revolutions
+  // Inturrupt for encoder A used to detect changes in revolutions
 }
 
-void loop() {
+void loop()
+{
   Home();
   Extend();
   Retract();
   HalfExtend();
 }
 
-void c() {
-  encoder++ ;
+void c()
+{
+  encoder++;
 }
 
-void cL() {
-  encoderL++ ;
+void cL()
+{
+  encoderL++;
 }
 
-void button(){
+void button()
+{
   eStop = !eStop;
 }
 
-void Home(){
-  if (digitalRead(23)==HIGH){ //Home
-    analogWrite(in1, 0);
-    analogWrite(in2,255);
-    analogWrite(in3, 0);
-    analogWrite(in4,255);
-    while (encoderlast != encoder && eStop == LOW){
+void Home()
+{
+  if (digitalRead(homePin) == HIGH)
+  { // Home
+    Serial.println("Home");
+    digitalWrite(HBridge_in1, LOW);
+    digitalWrite(HBridge_in2, HIGH);
+    digitalWrite(HBridge_in3, LOW);
+    digitalWrite(HBridge_in4, HIGH);
+    while (encoderlast != encoder)
+    { //&& eStop == LOW){
+      Serial.println("Home2");
       Serial.print(encoder);
       Serial.print(",  ");
       Serial.println(encoderlast);
       encoderlast = encoder;
-      analogWrite(in1, 0);
-      analogWrite(in2,255);
-      analogWrite(in3, 0);
-      analogWrite(in4,255);
+      digitalWrite(HBridge_in1, LOW);
+      digitalWrite(HBridge_in2, HIGH);
+      digitalWrite(HBridge_in3, LOW);
+      digitalWrite(HBridge_in4, HIGH);
       Serial.println(eStop);
       delay(500);
     }
-    analogWrite(in1, 0);
-    analogWrite(in2, 0);
-    analogWrite(in3, 0);
-    analogWrite(in4, 0);
+    digitalWrite(HBridge_in1, LOW);
+    digitalWrite(HBridge_in2, LOW);
+    digitalWrite(HBridge_in3, LOW);
+    digitalWrite(HBridge_in4, LOW);
     encoder = 0;
     state = 0;
     // eStop == LOW;
   }
 }
 
-void Extend(){
-  if (digitalRead(25)==HIGH){ //Extend
-    while (encoder <= 500 && eStop == LOW){
+void Extend()
+{
+  if (digitalRead(extendPin) == HIGH)
+  { // Extend
+    Serial.println("Extend");
+    while (encoder <= 500 && eStop == LOW)
+    {
       Serial.print(encoder);
       Serial.print(",  ");
       Serial.println(encoderlast);
-      analogWrite(in1, 255);
-      analogWrite(in2, 0);
-      analogWrite(in3, 255);
-      analogWrite(in4, 0);
+      digitalWrite(HBridge_in1, HIGH);
+      digitalWrite(HBridge_in2, 0);
+      digitalWrite(HBridge_in3, HIGH);
+      digitalWrite(HBridge_in4, LOW);
     }
-    analogWrite(in1,0);
-    analogWrite(in2, 0);
-    analogWrite(in3,0);
-    analogWrite(in4, 0);
+    digitalWrite(HBridge_in1, LOW);
+    digitalWrite(HBridge_in2, LOW);
+    digitalWrite(HBridge_in3, LOW);
+    digitalWrite(HBridge_in4, LOW);
     encoder = 0;
     state = 2;
     // eStop == LOW;
   }
 }
 
-void HalfExtend(){
-  if (digitalRead(29)==HIGH){ //Extend
-    while (encoder <= 320 && eStop == LOW){
+void HalfExtend()
+{
+  if (digitalRead(halfExtPin) == HIGH)
+  { // Extend
+    Serial.println("Half Extend");
+    while (encoder <= 320 && eStop == LOW)
+    {
       Serial.print(encoder);
       Serial.print(",  ");
       Serial.println(encoderlast);
-      analogWrite(in1, 255);
-      analogWrite(in2, 0);
-      analogWrite(in3, 255);
-      analogWrite(in4, 0);
+      digitalWrite(HBridge_in1, HIGH);
+      digitalWrite(HBridge_in2, LOW);
+      digitalWrite(HBridge_in3, HIGH);
+      digitalWrite(HBridge_in4, LOW);
     }
-    analogWrite(in1,0);
-    analogWrite(in2, 0);
-    analogWrite(in3,0);
-    analogWrite(in4, 0);
+    digitalWrite(HBridge_in1, LOW);
+    digitalWrite(HBridge_in2, LOW);
+    digitalWrite(HBridge_in3, LOW);
+    digitalWrite(HBridge_in4, LOW);
     encoder = 0;
     state = 1;
-    //eStop == LOW;
+    // eStop == LOW;
   }
 }
 
-void Retract(){
-  if (digitalRead(27)==HIGH && state == 2){ //Retract
-    while (encoder <= 500 && eStop == LOW){
+void Retract()
+{
+  if (digitalRead(retractPin) == HIGH && state == 2)
+  { // Retract
+    Serial.println("Retract");
+    while (encoder <= 500 && eStop == LOW)
+    {
       Serial.print(encoder);
       Serial.print(",  ");
       Serial.println(encoderlast);
-      analogWrite(in1, 0);
-      analogWrite(in2, 255);
-      analogWrite(in3, 0);
-      analogWrite(in4, 255);
+      digitalWrite(HBridge_in1, LOW);
+      digitalWrite(HBridge_in2, HIGH);
+      digitalWrite(HBridge_in3, LOW);
+      digitalWrite(HBridge_in4, HIGH);
     }
     eStop == LOW;
     Home();
   }
-  if (digitalRead(27)==HIGH && state == 1){ //Retract
-    while (encoder <= 250 && eStop == LOW){
+  if (digitalRead(retractPin) == HIGH && state == 1)
+  { // Retract
+    while (encoder <= 250 && eStop == LOW)
+    {
       Serial.print(encoder);
       Serial.print(",  ");
       Serial.println(encoderlast);
-      analogWrite(in1, 0);
-      analogWrite(in2, 255);
-      analogWrite(in3, 0);
-      analogWrite(in4, 255);
+      digitalWrite(HBridge_in1, LOW);
+      digitalWrite(HBridge_in2, HIGH);
+      digitalWrite(HBridge_in3, LOW);
+      digitalWrite(HBridge_in4, HIGH);
     }
     /* float x = 250;
-       while (encoderlast != encoder){
-       Serial.print(encoder);
-       Serial.print(",  ");
-       Serial.println(encoderlast);
-       encoderlast = encoder;
-       analogWrite(in3, 0);
-       analogWrite(in4, x);
-       x = x-5;
-       delay(200);
-       }
-       analogWrite(in3,0);
-       analogWrite(in4, 0);
-       encoder = 0;
-       }*/
-    //eStop == LOW;
+     while (encoderlast != encoder){
+         Serial.print(encoder);
+         Serial.print(",  ");
+         Serial.println(encoderlast);
+         encoderlast = encoder;
+         digitalWrite(HBridge_in3, LOW);
+         digitalWrite(HBridge_in4, x);
+         x = x-5;
+         delay(200);
+         }
+       digitalWrite(HBridge_in3,LOW);
+       digitalWrite(HBridge_in4, LOW);
+       encoder = LOW;
+      }*/
+    // eStop == LOW;
     Home();
   }
 }
