28f33d54a05e4a77b3bdb6ad3fd6fe225ecaed87
Alex Bethel <abethel@cs.nmt.edu>
2022-05-13 23:33:13 -0600

    File exceptions

--- user/abethel/gpio/rpi_gpio.cpp
+++ user/abethel/gpio/rpi_gpio.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <unistd.h>
 
 #include "rpi_gpio.hpp"
 
@@ -27,6 +28,7 @@ void GPIOPin::setdir_gpio(GPIODirection dir) {
   stringstream filename;
   filename << "/sys/class/gpio/gpio" << _num << "/direction";
   ofstream file(filename.str());
+  file.exceptions(ofstream::failbit | ofstream::badbit);
 
   switch (dir) {
   case GPIODirection::Input:
@@ -34,6 +36,9 @@ void GPIOPin::setdir_gpio(GPIODirection dir) {
   case GPIODirection::Output:
     file << "out";
   }
+
+  usleep(500000);
+  cout << "dir ok" << endl;
 }
 
 // Set the value of the pin, which must be in output mode.
@@ -41,6 +46,7 @@ void GPIOPin::setval_gpio(bool val) {
   stringstream filename;
   filename << "/sys/class/gpio/gpio" << _num << "/value";
   ofstream file(filename.str());
+  file.exceptions(ofstream::failbit | ofstream::badbit);
 
   file << (val ? "1" : "0");
 }
@@ -50,6 +56,7 @@ bool GPIOPin::getval_gpio() {
   stringstream filename;
   filename << "/sys/class/gpio/gpio" << _num << "/value";
   ifstream file(filename.str());
+  file.exceptions(ifstream::failbit | ifstream::badbit);
 
   string val;
   file >> val;
@@ -61,16 +68,22 @@ bool GPIOPin::getval_gpio() {
 void GPIOPin::export_gpio() {
   string filename = "/sys/class/gpio/export";
   ofstream file(filename);
+  file.exceptions(ofstream::failbit | ofstream::badbit);
 
   file << _num;
+  usleep(500000);
+  cout << "export ok" << endl;
 }
 
 // Unexports the GPIO pin.
 void GPIOPin::unexport_gpio() {
   string filename = "/sys/class/gpio/unexport";
   ofstream file(filename);
+  file.exceptions(ofstream::failbit | ofstream::badbit);
 
   file << _num;
+  usleep(500000);
+  cout << "unexport ok" << endl;
 }
 
 // Initialize communications with the numbered GPIO pin.
