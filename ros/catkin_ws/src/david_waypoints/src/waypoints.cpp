2f57d42a8877fd6715b7777b7d956a1cc5ae7d29
shastro <guitarshughes@hotmail.com>
2022-05-15 21:34:47 -0600

    Add camera utility program skeleton. Update Waypoints.

--- ros/catkin_ws/src/david_waypoints/src/waypoints.cpp
+++ ros/catkin_ws/src/david_waypoints/src/waypoints.cpp
@@ -3,6 +3,8 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <iostream>
+#include <opencv2/aruco.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 
@@ -10,13 +12,16 @@ using namespace std;
 
 class WaypointCalculator {
 public:
-  WaypointCalculator() {
+  WaypointCalculator(char *topic_name) {
     image_transport::ImageTransport it(_nh);
-    it.subscribe("/camera_image", 1, &WaypointCalculator::camera_image_callback,
+    it.subscribe(topic_name, 1, &WaypointCalculator::camera_image_callback,
                  this);
   }
 
-  void camera_image_callback(const sensor_msgs::ImageConstPtr &ros_image) {}
+  void camera_image_callback(const sensor_msgs::ImageConstPtr &ros_image) {
+
+  }
+
 
 private:
   ros::NodeHandle _nh;
@@ -24,9 +29,14 @@ private:
 };
 
 int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    cout << "Usage waypoints <image_topic>" << endl;
+    abort();
+  }
   ros::init(argc, argv, "waypoints");
 
-  WaypointCalculator calc;
+  WaypointCalculator calc(argv[1]);
   ros::spin();
 
   ros::shutdown();
---
+++ utils/camera/calibration/take_pic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <sstream>
+
+
+int main(void){
+
+  cv::Mat frame;
+
+
+
+
+
+
+
+  return 0;
+}
